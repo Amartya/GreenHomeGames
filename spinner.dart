42f8bcf62aaefa07221090b51eaf2c1a682fd52d
Michael Horn <michael-horn@northwestern.edu>
2013-01-28 23:18:49 -0600

    Refactored to own its own canvas and events

--- spinner.dart
+++ spinner.dart
@@ -15,11 +15,148 @@ part of GreenHomeGames;
 
 
 /*
+ * Combined date spinner with month and day
+ */
+class DateSpinner {
+  
+  // Month spinner
+  Spinner mspin;
+  
+  // Day of month spinner
+  Spinner dspin;
+
+  // dimensions on the screen  
+  int width = 320, height = 60;
+  
+  // callback for spin completion
+  Function onDone = null;
+  
+  // Drawing context
+  CanvasRenderingContext2D ctx;
+  
+  // Is the mouse down
+  bool mdown = false;
+  
+  // Reference to the game engine
+  Game game;
+  
+  
+  DateSpinner(this.game) {
+    
+    // Load canvas and drawing context
+    CanvasElement canvas = document.query("#spinner");
+    width = canvas.width;
+    height = canvas.height;
+    ctx = canvas.getContext("2d");
+
+    // create the month spinner    
+    mspin = new Spinner(0, 0, 200, height, ctx);
+    mspin.VALUES = Weather.MONTHS;
+    mspin.onDone = () {
+      game.hideWeather();
+    };
+
+    // create day spinner    
+    dspin = new Spinner(220, 0, 100, height, ctx);
+    setDayValues();
+    dspin.onDone = () {
+      game.setDate(mspin.index + 1, dspin.index + 1);
+      game.showWeather();
+    };
+    
+    // Register mouse events
+    canvas.on.mouseDown.add((e) => mouseDown(e), true);
+    canvas.on.mouseUp.add((e) => mouseUp(e), true);
+    canvas.on.mouseMove.add((e) => mouseMove(e), true);
+
+    // Register touch events
+    canvas.on.touchStart.add((e) => touchDown(e), true);
+    canvas.on.touchMove.add((e) => touchDrag(e), true);
+    canvas.on.touchEnd.add((e) => touchUp(e), true);
+  }
+  
+  
+  void setDate(int month, int day) {
+    mspin.setIndex(month - 1);
+    setDayValues();
+    dspin.setIndex(day - 1);
+  }
+  
+  
+  int get month => mspin.index + 1;
+  
+  int get day => dspin.index + 1;
+  
+  
+  void draw() {
+    mspin.draw();
+    dspin.draw();
+  }
+  
+  
+  void setDayValues() {
+    int m = mspin.index + 1;
+    int d = 31;
+    
+    // 30 days hath September ...
+    if (m == 9 || m == 4 || m == 6 || m == 11) {
+      d = 30;
+    } else if (m == 2) {
+      d = 28;
+    }
+    
+    dspin.VALUES = [];
+    for (int i=1; i<=d; i++) dspin.VALUES.add(i);
+  }
+  
+  
+  void mouseUp(MouseEvent evt) {
+    mdown = false;
+  }
+  
+  
+  void mouseDown(MouseEvent evt) {
+    if (evt.offsetX <= 200) {
+      mspin.moveTo(mspin.index + 1);
+      setDayValues();
+    } else if (evt.offsetX >= 220) {
+      dspin.spin();
+    }
+    mdown = true;
+  }
+  
+  
+  void mouseMove(MouseEvent evt) {
+    if (mdown) {
+    }
+  }
+  
+  
+  void touchDown(TouchEvent tframe) {
+    for (var te in tframe.changedTouches) {
+    }
+  }
+  
+  
+  void touchUp(var tframe) {
+    for (var te in tframe.changedTouches) {
+    }
+  }
+  
+  
+  void touchDrag(var tframe) {
+    for (var te in tframe.changedTouches) {
+    }
+  }
+}
+
+
+/*
  * Number spinner for month and day
  */
 class Spinner {
    
-  var VALUES = [ "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC" ];
+  var VALUES = [];
    
   Random rand = new Random();
   
@@ -37,19 +174,22 @@ class Spinner {
    
   // angular force for spins
   double force = 0.0;
+  
+  // callback for when the spin is complete
+  Function onDone = null;
+  
+  // canvas rendering context
+  CanvasRenderingContext2D ctx;
+  
    
-  // alignment ("left" or "right")
-  String align = "left";
-   
-   
-  Spinner(this.x, this.y, this.width, this.height);
+  Spinner(this.x, this.y, this.width, this.height, this.ctx);
   
 
 /*
  * Randomly spin to a new index
  */
   void spin() {
-    force = 0.15 + rand.nextDouble() * 0.1;
+    force = 0.15 + rand.nextDouble() * 0.15;
     animate();
   }
    
@@ -66,10 +206,21 @@ class Spinner {
     tween.addControlPoint(incr * delta + incr * 0.02, 1);
     tween.duration = min(delta.abs() * 15, 60);
     tween.ondelta = (value) { angle += value; animate(); };
-    tween.onend = () { angle = incr * index; animate(); };
+    tween.onend = () {
+      angle = incr * index;
+      animate();
+      if (onDone != null) onDone();
+    };
     tween.play();
   }
    
+  
+  void setIndex(int i) {
+    index = i;
+    double incr = PI * 2 / VALUES.length;
+    angle = incr * index;
+  }
+  
    
   void animate() {
     double incr = PI * 2 / VALUES.length;
@@ -80,6 +231,9 @@ class Spinner {
         force = 0.0;
         angle = incr * index;
         Sounds.playSound("tick");
+        if (onDone != null) {
+          onDone();
+        }
       }
       window.setTimeout(animate, 30);
     }
@@ -97,34 +251,37 @@ class Spinner {
 
    
   void draw() {
-    CanvasRenderingContext2D ctx = Game.ctx;
-
     ctx.save();
-      
-    ctx.fillStyle = "#223";
-    ctx.fillRect(x, y, width, height);
+    ctx.translate(x, y);
+    
+    ctx.clearRect(0, 0, width, height);
+    ctx.fillStyle = "rgba(255, 255, 255, 0.95)"; //"rgba(0, 0, 0, 0.3)";
+    ctx.fillRect(0, 0, width, height);
      
     ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x + width, y);
-    ctx.lineTo(x + width, y + height);
-    ctx.lineTo(x, y + height);
+    ctx.moveTo(0, 0);
+    ctx.lineTo(width, 0);
+    ctx.lineTo(width, height);
+    ctx.lineTo(0, height);
     ctx.closePath();
     ctx.clip();
+    
+    ctx.strokeStyle = "black";
+    ctx.strokeRect(0, 0, width, height);
      
-    ctx.font = "30px 'Share Tech', sans-serif";
+    ctx.font = "46px sans-serif";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
-    ctx.fillStyle = "white";
-    ctx.shadowColor = "black";
+    ctx.fillStyle = "black"; //"white";
+    ctx.shadowColor = "#666"; //"black";
     ctx.shadowOffsetX = 1;
     ctx.shadowOffsetY = 1;
-    ctx.shadowBlur = 4;
+    ctx.shadowBlur = 2;
       
-    double r = (align == "right") ? -8.0 : 8.0;
+    double r = 16.0;
     r *= VALUES.length;
     ctx.save();
-    ctx.translate(x + width/2 - r, y + height/2);
+    ctx.translate(width/2 - r, height/2);
     ctx.rotate(angle);
     for (int i=0; i<VALUES.length; i++) {
       ctx.fillText(VALUES[i].toString(), r, 2);
@@ -134,7 +291,7 @@ class Spinner {
       
     ctx.lineWidth = 1;
     ctx.strokeStyle = "black";
-    ctx.strokeRect(x, y, width, height);
+    ctx.strokeRect(0, 0, width, height);
     
     ctx.restore();  // from clip
   }
