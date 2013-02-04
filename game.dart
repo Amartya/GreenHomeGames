5e78fb883c1b5d7b30496b4522d292806b00b2b6
Michael Horn <michael-horn@northwestern.edu>
2013-02-04 13:43:31 -0600

    WIP. Not ready for production. Needs major re-org.

--- game.dart
+++ game.dart
@@ -27,12 +27,37 @@ class Game {
 
   // indoor temperature simulation
   Simulator simulator;
-   
-  // Status indicators
-  Indicator pollution, energy, money;
+
+  // Penguin family
+  Family family;
+  
+  // Energy bill
+  int curr_bill = 0;
+  int prev_bill = 0;
   
   
   Game() {
+
+    // Family of penguins    
+    family = new Family(this);
+    
+    // Round thermostat control
+    thermostat = new RoundThermostat(this);
+    
+    // Weather model
+    weather = new Weather();
+    
+    // Temperature simulation
+    simulator = new Simulator(this);
+    simulator.onDone = () {
+      prev_bill += curr_bill;
+      curr_bill = simulator.bill;
+      updateBill();
+    };
+    simulator.onStart = () {
+      Element el = document.query("#paid-stamp");
+      if (el != null) el.style.visibility = "hidden";
+    };
     
     // Load sound effects
     Sounds.loadSound("tick");
@@ -45,93 +70,104 @@ class Game {
       weather.setDate(spinner.month, spinner.day);
     };
 
-    // Transition from page1 to page2
-    ButtonElement next = document.query("#next1");
-    next.on.click.add((event) {
-      slidePageOut("page1");
-      slidePageIn("page2");
-      next.style.visibility = "hidden";
-    }, true);
+
+    // set up the navigation buttons    
+    var buttons = document.queryAll(".next-button");
+    for (ButtonElement button in buttons) {
+      button.on.click.add((event) {
+        int page = int.parse(button.value);
+        int next = page + 1;
+        if (next > 4) next = 1;
+        slidePageOut("page${page}", true);
+        slidePageIn("page${next}", true);
+      }, true);
+    }
     
     
+    buttons = document.queryAll(".back-button");
+    for (ButtonElement button in buttons) {
+      button.on.click.add((event) {
+        int page = int.parse(button.value);
+        int prev = page - 1;
+        if (prev < 1) prev = 4;
+        slidePageOut("page${page}", false);
+        slidePageIn("page${prev}", false);
+      }, true);
+    }
+    
+    ButtonElement button = document.query("#pay-now");
+    if (button != null) {
+      button.on.click.add((event) {
+        Element el = document.query("#paid-stamp");
+        el.style.visibility = "visible";
+        prev_bill = 0;
+        curr_bill = 0;
+        //updateBill();
+      }, true);
+    }
+
     // Transition from page2 to page3
-    next = document.query("#next2");
-    next.on.click.add((event) {
-      slidePageOut("page2");
-      slidePageIn("page3");
-      window.setTimeout(() {
-        simulator.clear();
-        simulator.run();
-      }, 1000);
-    }, true);
+    //next = document.query("#next2");
+    //back = document.query("#back2");
+    //next.on.click.add((event) {
+    //  slidePageOut("page2");
+    //  slidePageIn("page3");
+    //  window.setTimeout(() {
+    //    simulator.clear();
+    //    simulator.run();
+    //  }, 1000);
+    //}, true);
     
-    
-    // Transition from page3 to page1
-    next = document.query("#next3");
-    next.on.click.add((event) {
-      slidePageOut("page3");
-      slidePageIn("page1");
-      next.style.visibility = "hidden";
-    }, true);
-    
-    
-    // Pollution indicator
-    /*
-    pollution = new Indicator(450.0, 225.0);
-    pollution.percent = 75;
-    pollution.total = 235;
-    */
-    
-    // Energy indicator
-    /*
-    energy = new Indicator(450.0, 325.0);
-    energy.percent = 60;
-    energy.total = 180;
-    energy.label = "Energy";
-    energy.unit = "kilowatt hours";
-    */
-    
-    // Money indicator
-    /*
-    money = new Indicator(450.0, 425.0);
-    money.percent = 92;
-    money.total = 150;
-    money.label = "Money";
-    money.unit = "dollars";
-    */
-    
-    // Temperature simulation
-    simulator = new Simulator(this);
-    simulator.onDone = () {
-      Element el = document.query("#next3");
-      el.style.visibility = 'visible';
-    };
-    
-    // Round thermostat control
-    thermostat = new RoundThermostat(this);
-    
-    // Weather model
-    weather = new Weather();
     setDate(1, 1);
-    hideWeather();
+    showWeather();
     draw();
     
     // slide in page 1
-    window.setTimeout(() => slidePageIn("page1"), 1000);
+    window.setTimeout(() => slidePageIn("page1", true), 1000);
   }
   
   
-  void slidePageIn(String page) {
-    Element el = document.query("#$page");
-    el.style.animation = "slidein 0.5s ease-in 0 1";
-    el.style.left = "25px";
+  void updateBill() {
+    setText("bill-subtotal", "\$${curr_bill}");
+    setText("bill-previous", "\$${prev_bill}");
+    setText("bill-total", "\$${prev_bill + curr_bill}");
+    setText("bill-energy", "${simulator.energy.toInt()} kWh");
+    setText("bill-summary-amount", "\$${prev_bill + curr_bill}");
+    setText("bill-summary-date", "${weather.getDateString()}");
+    double rate = double.parse(window.localStorage['valueC']);
+    setText("bill-rate", "${rate}");
   }
   
   
-  void slidePageOut(String page) {
+  void setText(String id, String text) {
+    Element el = document.query("#${id}");
+    if (el != null) {
+      el.innerHtml = text;
+    }
+  }
+  
+  
+  void slidePageIn(String page, bool left) {
     Element el = document.query("#$page");
-    el.style.animation = "slideout 0.5s ease-out 0 1";
-    el.style.left = "-1500px";
+    if (left) {
+      el.style.animation = "slidein-left 0.5s ease-in 0 1";
+      el.style.left = "25px";
+    } else {
+      el.style.animation = "slidein-right 0.5s ease-in 0 1";
+      el.style.left = "25px";
+    }
+  }
+  
+  
+  void slidePageOut(String page, bool left) {
+    Element el = document.query("#$page");
+    if (left) {
+      el.style.animation = "slideout-left 0.5s ease-out 0 1";
+      el.style.left = "-1500px";
+    } else {
+      el.style.animation = "slideout-right 0.5s ease-out 0 1";
+      el.style.left = "1500px";
+    }
   }
   
   
@@ -145,20 +181,15 @@ class Game {
   
   void hideWeather() {
     Element el;
-    el = document.query("#next1");
-    el.style.visibility = 'hidden';
-    el = document.query("#high-temp");
-    el.innerHtml = "?";
-    el = document.query("#low-temp");
-    el.innerHtml = "?";
-    el = document.query("#weather-report");
-    el.innerHtml = "";
-    el = document.query("#weather-temps");
-    el.innerHtml = "";
+    setText("high-temp", "?");
+    setText("low-temp", "?");
+    setText("weather-report", "");
+    setText("weather-temps", "");
   }
   
   
   void showWeather() {
+    
     Element el = document.query("#high-temp");
     el.innerHtml = "${weather.high}&deg;";
     el = document.query("#low-temp");
@@ -167,8 +198,8 @@ class Game {
     el.innerHtml = weather.getWeatherSummary();
     el = document.query("#weather-temps");
     el.innerHtml = weather.getTemperatureString();
-    el = document.query("#next1");
-    el.style.visibility = 'visible';
+    simulator.clear();
+    family.resetComfortScores();
   }
 
   
